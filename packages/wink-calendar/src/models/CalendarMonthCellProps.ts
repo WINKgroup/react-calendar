15b41f863be47d516b8f3191c33d58a6eadc8ce0
Alen Ajam <alen.ajam@live.com>
2021-12-03 10:06:50 +0100

    added documentation

--- packages/wink-calendar/src/models/CalendarMonthCellProps.ts
+++ packages/wink-calendar/src/models/CalendarMonthCellProps.ts
@@ -1,15 +1,22 @@
 import { Timestamp } from './Date';
 import { CalendarEvent } from './CalendarEvent';
 
+/** <CalendarMonthCell /> */
 export type CalendarMonthCellProps = {
+  /** Date which this cell represents */
   date: Timestamp;
+  /** Configuration object */
   config?: CalendarMonthCellConfig;
+  /** Events to be shown inside of this cell */
   events?: CalendarEvent[];
+  /** Triggered when cell gets clicked */
   onClick?: () => void;
 };
 
 export type CalendarMonthCellConfig = {
+  /** Disables click behaviour on the cell */
   disabled?: boolean;
+  /** Flags to customize style of the cell */
   style?: {
     active?: boolean;
     opaque?: boolean;
--- packages/wink-calendar/src/models/CalendarMonthHeaderProps.ts
+++ packages/wink-calendar/src/models/CalendarMonthHeaderProps.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 
+/** <CalendarMonthHeader /> */
 export type CalendarMonthHeaderProps = {
   currentMonth: DateTime;
   onChangeMonth?: (month: DateTime) => void;
